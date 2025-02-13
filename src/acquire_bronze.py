a027cd4b2283d30ea203eb4a2a80e7795465e6ed
Alan Avelar <alan.avelar95@gmail.com>
2025-02-13 18:49:16 -0300

    Finalizando job da bronze e ajustando para parquet

--- src/acquire_bronze.py
+++ src/acquire_bronze.py
@@ -1,8 +1,7 @@
-# Extração de dados da API Coingecko e salvamento no GCP
 import requests
 import os
-import json
-from pprint import pprint
+import pandas as pd
+import io
 from dotenv import load_dotenv
 from google.cloud import storage
 from datetime import datetime, timezone, timedelta
@@ -10,55 +9,62 @@ from datetime import datetime, timezone, timedelta
 # Carregar variáveis de ambiente
 load_dotenv()
 os.environ["COINGECKO_API_KEY"] = os.getenv("COINGECKO_API_KEY")
-os.environ["GCP_BUCKET_NAME"] = os.getenv("GCP_BUCKET_NAME")
+os.environ["BRONZE_BUCKET_NAME"] = os.getenv("BRONZE_BUCKET_NAME")
 
 credentials_path = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 base_dir = os.path.dirname(os.path.dirname(__file__))
 absolute_credentials_path = os.path.join(base_dir, credentials_path)
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = absolute_credentials_path
-print(os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
 
 def extract_data_from_coingecko():
     url = "https://api.coingecko.com/api/v3/coins/markets"
 
     params = {
-        "vs_currency": "usd",           # Moeda de referência
-        "order": "market_cap_desc",     # Ordenar por capitalização de mercado
-        "per_page": 100,                  # Quantidade de criptomoedas
-        "page": 1,                      # Página inicial
+        "vs_currency": "usd",
+        "order": "market_cap_desc",
+        "per_page": 100,
+        "page": 1,
         "x_cg_demo_api_key": os.environ.get("COINGECKO_API_KEY")
     }
+    
     headers = {"accept": "application/json"}
     response = requests.get(url, headers=headers, params=params)
-    
+
     if response.status_code == 200:
-        data = response.json()
-        return data
+        return response.json()
     else:
         print(f"Error on fetching API data: {response.status_code}")
         print(f"Error object: {response.json()}")
-        return {}
+        return []
 
-def upload_to_bronze(bucket_name, data, blob_name):
+def upload_parquet_to_bronze(bucket_name, data, destination_blob_name):
     try:
         client = storage.Client()
         bucket = client.bucket(bucket_name)
-        blob = bucket.blob(blob_name)
-        blob.upload_from_string(json.dumps(data, indent=4, ensure_ascii=False), content_type="application/json")
-        print(f"File sent to bronze layer on bucket '{bucket_name}/{blob_name}'.")
+        blob = bucket.blob(destination_blob_name)
+
+        df = pd.DataFrame(data)
+
+        parquet_buffer = io.BytesIO()
+        df.to_parquet(parquet_buffer, engine="pyarrow", index=False)
+        parquet_buffer.seek(0)
+
+        blob.upload_from_file(parquet_buffer, content_type="application/octet-stream")
+        
+        print(f"File sent to bronze layer on bucket '{bucket_name}/{destination_blob_name}'.")
     except Exception as e:
         print(f"Error when uploading to Cloud Storage: {e}")
 
 if __name__ == "__main__":
     data = extract_data_from_coingecko()
-    
-    timestamp = (datetime.now(timezone.utc) - timedelta(hours=3)).strftime("%Y%m%d%H%M%S")
-    print(timestamp)
+    timestamp = (datetime.now(timezone.utc) - timedelta(hours=3))
+    year, month, day = timestamp.strftime("%Y"), timestamp.strftime("%m"), timestamp.strftime("%d")
+    formatted_timestamp = timestamp.strftime("%Y%m%d%H%M%S")
 
     bronze_bucket_name = os.getenv("BRONZE_BUCKET_NAME")    
-    bronze_blob_name = f"coingecko/markets_{timestamp}.json"
+    bronze_blob_name = f"coingecko/{year}/{month}/{day}/markets_{formatted_timestamp}.parquet"
 
-    if bronze_bucket_name:
-        upload_to_bronze(bronze_bucket_name, data, bronze_blob_name)
+    if data and bronze_bucket_name:
+        upload_parquet_to_bronze(bronze_bucket_name, data, bronze_blob_name)
     else:
-        print("GCP_BUCKET_NAME not properly set on .env")
+        print("No data extracted or BRONZE_BUCKET_NAME not properly set on .env")
